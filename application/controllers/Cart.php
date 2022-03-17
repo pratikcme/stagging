c93b54fc569afdc50bff318874609ed9413dcfb1
shahidCME <sahid.cmexpertise@gmail.com>
2022-03-17 17:00:49 +0530

    file change by SAR2

--- application/controllers/Cart.php
+++ application/controllers/Cart.php
@@ -4,7 +4,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Cart extends User_Controller {
 
 	function __construct(){
-		echo "hello";
 		parent::__construct();
 		$this->controller = $this->myvalues->cartFrontEnd['controller'];
 		$this->url = SITE_URL . 'frontend/'. $this->controller;
