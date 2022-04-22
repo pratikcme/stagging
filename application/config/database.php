61eee74d56ea10f66d410ab2afe2cdd9d8a5b421
pratikcme <pratik.cmexpertise@gmail.com>
2022-04-22 16:56:32 +0530

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -80,12 +80,10 @@ if($_SERVER['HTTP_HOST'] == '192.168.2.2' || $_SERVER['HTTP_HOST'] == 'localhost
 	$host = 'localhost';
 	
 }else{
-	
-$host = 'localhost';
-$userName = 'a1630btr_development_user';
-$password = 'Ra#Ch^1j.sLR';
-$dbName = 'a1630btr_bigbucket';
-	
+	$host = 'localhost';
+	$userName = 'a1630btr_development_user';
+	$password = 'Ra#Ch^1j.sLR';
+	$dbName = 'a1630btr_bigbucket';	
 }
 
 // echo $dbName;die;
