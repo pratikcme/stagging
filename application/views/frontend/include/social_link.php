a2631aa711b1f79cbfdf8ff3e81800ed94915a50
pratikcme <pratik.cmexpertise@gmail.com>
2022-05-12 16:16:32 +0530

    Update social_link.php

--- application/views/frontend/include/social_link.php
+++ application/views/frontend/include/social_link.php
@@ -8,11 +8,11 @@
       </div>
       <div class="col-md-6">
         <ul class="social-icon">
-          <li style="display: <?=($appLinks[0]->facebook_link == '') ? 'none' : '' ?>"><a href="<?=($appLinks[0]->facebook_link != '') ? $appLinks[0]->facebook_link : 'javascript:' ?>"><i class="fab fa-facebook-f"></i></a></li>
-          <li style="display: <?=($appLinks[0]->twitter_link == '') ? 'none' : '' ?>"><a href="<?=($appLinks[0]->twitter_link != '') ? $appLinks[0]->twitter_link : 'javascript:' ?>"><i class="fab fa-twitter"></i></a></li>
+          <li style="display: <?=(@$appLinks[0]->facebook_link == '') ? 'none' : '' ?>"><a href="<?=(@$appLinks[0]->facebook_link != '') ? @$appLinks[0]->facebook_link : 'javascript:' ?>"><i class="fab fa-facebook-f"></i></a></li>
+          <li style="display: <?=(@$appLinks[0]->twitter_link == '') ? 'none' : '' ?>"><a href="<?=(@$appLinks[0]->twitter_link != '') ? @$appLinks[0]->twitter_link : 'javascript:' ?>"><i class="fab fa-twitter"></i></a></li>
           <!-- <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li> -->
-          <li style="display: <?=($appLinks[0]->google_plus_link == '') ? 'none' : '' ?>"><a href="<?=($appLinks[0]->google_plus_link != '') ? $appLinks[0]->google_plus_link : 'javascript:' ?>"><i class="fab fa-google-plus-g"></i></a></li>
-          <li style="display: <?=($appLinks[0]->instagram_link == '') ? 'none' : '' ?>"><a href="<?=($appLinks[0]->instagram_link != '') ? $appLinks[0]->instagram_link : 'javascript:' ?>"><i class="fab fa-instagram"></i></a></li>
+          <li style="display: <?=(@$appLinks[0]->google_plus_link == '') ? 'none' : '' ?>"><a href="<?=(@$appLinks[0]->google_plus_link != '') ? @$appLinks[0]->google_plus_link : 'javascript:' ?>"><i class="fab fa-google-plus-g"></i></a></li>
+          <li style="display: <?=(@$appLinks[0]->instagram_link == '') ? 'none' : '' ?>"><a href="<?=(@$appLinks[0]->instagram_link != '') ? @$appLinks[0]->instagram_link : 'javascript:' ?>"><i class="fab fa-instagram"></i></a></li>
         </ul>
       </div>
     </div>
