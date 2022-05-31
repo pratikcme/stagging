59193c6d02eca1e93497483b337ad849894d9b4b
pratikcme <101399602+pratikcme@users.noreply.github.com>
2022-05-31 19:32:19 +0530

    Update Promocode_manage_model.php

--- application/models/Promocode_manage_model.php
+++ application/models/Promocode_manage_model.php
@@ -66,7 +66,7 @@ class Promocode_manage_model extends My_model{
         $data['update'] = $update;
         $data['where'] = ['id'=>$this->id];
         $res = $this->updateRecords($data);
-        
+        lq();
         if($res){
             $jsone_response['status'] = 'success';
             $jsone_response['message'] = 'Data updated success!!!';
