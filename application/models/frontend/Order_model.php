997db25df532dbc9a49a16b94f79cd771ec8fb31
pratikcme <101399602+pratikcme@users.noreply.github.com>
2022-06-06 13:09:29 +0530

    Update Order_model.php

--- application/models/frontend/Order_model.php
+++ application/models/frontend/Order_model.php
@@ -131,6 +131,7 @@ Class Order_model extends My_model{
             $userlat = $userAddressLatLong[0]->latitude;
             $userlong = $userAddressLatLong[0]->longitude;
             $delivery_charge = $this->getDeliveryCharge($userlat,$userlong,$branch_id);
+            echo "<das>";
         }
 
             dd($delivery_charge);die;
