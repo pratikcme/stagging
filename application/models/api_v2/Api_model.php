ccf47f9e594f145977725ec5f033f122d60141cb
shahidCME <sahid.cmexpertise@gmail.com>
2022-06-15 15:26:08 +0530

    Update Api_model.php

--- application/models/api_v2/Api_model.php
+++ application/models/api_v2/Api_model.php
@@ -3654,7 +3654,6 @@ class Api_model extends My_model {
         $data['select'] = ['od.*','pw.id as product_varient_id','pw.price','pw.price','pw.discount_price','pw.weight_no','w.name as weight_name','pw.discount_per','pkg.package as package_name','pw.max_order_qty','p.name','pw.product_id','p.branch_id','pw.quantity as available_quantity','pw.price as actual_price','w.id as weight_id'];
         $data['where'] = ['od.offer_id'=>$postData['offer_id']];
         $return =  $this->selectFromJoin($data);
-        lq();
         unset($data);
         $product_variant_id = $return[0]->product_varient_id;
         $branch_id = $return[0]->branch_id;
