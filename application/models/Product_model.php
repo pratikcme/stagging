b7720e89d0bbfc6fe4581b4ebc115c919417bc4d
shahidCME <sahid.cmexpertise@gmail.com>
2022-06-21 10:41:28 +0530

    Update Product_model.php

--- application/models/Product_model.php
+++ application/models/Product_model.php
@@ -548,8 +548,7 @@ public function Product_add_update(){
                     $path = 'public/images/'.$this->folder.'product_image/';
                     foreach ($files['userfile']['name'] as $key => $value) {
 
-                        $_FILES['userfile']['name'] = str_replace(' ', '',$files['userfile']['name'][$key]); 
-                        dd($_FILES['userfile']['name']);
+                        $_FILES['userfile']['name'] = $files['userfile']['name'][$key];
                         $_FILES['userfile']['type'] = $files['userfile']['type'][$key]; 
                         $_FILES['userfile']['tmp_name'] = $files['userfile']['tmp_name'][$key]; 
                         $_FILES['userfile']['error'] = $files['userfile']['error'][$key]; 
