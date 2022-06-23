f44d747c5e9ea522970d4c6e8d19254efaed2ebd
shahidCME <sahid.cmexpertise@gmail.com>
2022-06-23 12:14:13 +0530

    Update Delivery_api.php

--- application/controllers/api_v2/Delivery_api.php
+++ application/controllers/api_v2/Delivery_api.php
@@ -167,6 +167,7 @@ class Delivery_api extends CI_Controller
     public function order_delivered()
     {
         $postdata = $this->input->post();
+        dd($postdata);
         if (isset($postdata['order_id'])) {
 
             $result = $this->this_model->order_delivered($postdata);
