62c5c9b6927bbb05ac1e80c57d635eba7f9fb515
shahidCME <sahid.cmexpertise@gmail.com>
2022-06-23 16:35:51 +0530

    Update Offer.php

--- application/controllers/Offer.php
+++ application/controllers/Offer.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Offer extends CI_Controller{
+class Offer extends Admin_Controller{
 
 	function __construct(){
 		parent::__construct();
