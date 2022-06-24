60151c9caa4dad968508388f8ff1a02330cce727
shahidCME <sahid.cmexpertise@gmail.com>
2022-06-24 12:02:07 +0530

    Update Delivery_api_model.php

--- application/models/api_v2/Delivery_api_model.php
+++ application/models/api_v2/Delivery_api_model.php
@@ -414,7 +414,7 @@ class Delivery_api_model extends My_model
         $data['table'] = 'delivery_notification';
         $this->deleteRecords($data);
         
-        $iOrderNo = $order_data[0]->branch_id;
+        $iOrderNo = $order_data[0]->order_no;
         $message = 'Order '.$iOrderNo .' is Delivered' ;
         $branchNotification = array(
             'order_id'         =>  $order_id,
