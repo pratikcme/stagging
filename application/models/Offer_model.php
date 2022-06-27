9be4bb3e0cbe0b47aaa599dc08e4d5b1274b1cdf
shahidCME <sahid.cmexpertise@gmail.com>
2022-06-27 18:13:17 +0530

    Update Offer_model.php

--- application/models/Offer_model.php
+++ application/models/Offer_model.php
@@ -8,7 +8,7 @@ Class Offer_model extends My_model{
         $server_name = $_SERVER['SERVER_NAME'];
         // $this->crone_url = $request_schema.'://'.$server_name."/offer/applied_offer_bycron";
         $this->crone_url = $request_schema.'://'.$server_name."/stagging/cron/test";
-        $this->crone_url_local = $request_schema.'://'.$server_name."/stagging/cron/test";
+        $this->crone_url_local = $request_schema.'://'.$server_name."/stagging/cron/test"; 
     }
 
 
