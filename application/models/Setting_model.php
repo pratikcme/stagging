a9e36657c789ec8bc1116b3b77b449ea94b09153
shahidCME <sahid.cmexpertise@gmail.com>
2022-06-30 16:03:46 +0530

    Update Setting_model.php

--- application/models/Setting_model.php
+++ application/models/Setting_model.php
@@ -113,7 +113,6 @@ class Setting_model extends My_model{
         $data['select'] = ['*'];
         $data['where'] = ['request_id'=>'2','vendor_id'=>$this->vendor_id];
         $result = $this->selectRecords($data,true);
-        lq();
         return $result[0]; 
     }
 }
