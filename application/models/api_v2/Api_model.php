0a7e79dee09cad4bcaa98eb891818e7862b2b131
shahidCME <sahid.cmexpertise@gmail.com>
2022-06-30 18:13:01 +0530

    Update Api_model.php

--- application/models/api_v2/Api_model.php
+++ application/models/api_v2/Api_model.php
@@ -3533,6 +3533,7 @@ class Api_model extends My_model {
         unset($data);
         foreach ($result as $k => $v) {
             if($v->end_date == $today && $v->end_time >= $time){
+                echo '1';die;
                 unset($result[$key]);
                 continue;
             }
