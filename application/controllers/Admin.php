8486787b12826987c49fe52ead805b32fdd040d9
shahidCME <sahid.cmexpertise@gmail.com>
2022-06-30 16:53:14 +0530

    Update Admin.php

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -360,7 +360,7 @@ class Admin extends CI_Controller
            $server_name = $_SERVER["SERVER_NAME"];
            $result_login = $this->db->query("SELECT * FROM `vendor` WHERE email='$email' AND password='$password' AND server_name='$server_name'"); 
             $row_login = $result_login->row_array();
-            lq();
+            dd($row_login);
         if ($result_login->num_rows() > 0) {
            
                 $login_data = array(
