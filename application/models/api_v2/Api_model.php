57b60e3746851926daf3823633b9357add8a0d30
shahidCME <sahid.cmexpertise@gmail.com>
2022-07-04 15:58:10 +0530

    Update Api_model.php

--- application/models/api_v2/Api_model.php
+++ application/models/api_v2/Api_model.php
@@ -56,7 +56,7 @@ class Api_model extends My_model {
         $data['table'] = 'user';
         $getUser = $this->selectRecords($data);
         // lq();
-        dd($getUser);
+        // dd($getUser);
 
         if(empty($getUser)){
             $checkAlreadyRegisterWithEmail = $this->check_register($postData['email'],$postData['vendor_id']);
@@ -90,6 +90,7 @@ class Api_model extends My_model {
             }else{
                 $this->insertRecord($dataIns);
             }
+            lq();
             if($postData['login_type']=='0'){
                 $response["success"] = 1;
                 $response["message"] = "Account created successfully";
@@ -132,7 +133,7 @@ class Api_model extends My_model {
         $data['limit'] = 1;
         $data['table'] = 'user';        
         $getUser =  $this->selectRecords($data,true);
-       
+
         print_r($getUser);
         return $this->sendLoginResponse($getUser[0],$postData);
 
