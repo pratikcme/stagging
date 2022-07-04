ae2ac7d6a485528eeb275e6dea8d2c29c27e325a
shahidCME <sahid.cmexpertise@gmail.com>
2022-07-04 16:22:40 +0530

    Update Api_model.php

--- application/models/api_v2/Api_model.php
+++ application/models/api_v2/Api_model.php
@@ -3534,11 +3534,11 @@ class Api_model extends My_model {
                 unset($result[$k]);
                 continue;
             }
-            $c = $this->check_branch_is_active($v->branch_id);
-            if($c[0]->status == '0'){
-                unset($result[$k]);
-                continue;   
-            }
+            // $c = $this->check_branch_is_active($v->branch_id);
+            // if($c[0]->status == '0'){
+            //     unset($result[$k]);
+            //     continue;   
+            // }
             $v->image = base_url() . 'public/images/'.$this->folder.'offer_image/' . $v->image;
             $data['select'] = ['c.name as category_name','p.category_id','pw.product_id'];
             $data['table'] = TABLE_PRODUCT_WEIGHT . ' as pw';
