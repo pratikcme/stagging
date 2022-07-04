796b15c1027bcf8d12120500f3c3fae33d374b03
shahidCME <sahid.cmexpertise@gmail.com>
2022-07-04 13:18:12 +0530

    Update checkout.js

--- public/frontend/assets/javascript/checkout.js
+++ public/frontend/assets/javascript/checkout.js
@@ -486,6 +486,8 @@ var CHECKOUT = function(){
                             $('#mobileModal').modal('hide');
                             $('#Otp').modal('show');
                             $('#Otp').modal({ backdrop: 'static', keyboard: false });
+                        }else{
+                            $('.mobile_verfication').html('This mobile number is linked with another account');
                         }
                     }            
                 });
