7fcc3d411d3dc3541267ef10e3eacddb190bd58a
shahidCME <sahid.cmexpertise@gmail.com>
2022-07-04 19:40:28 +0530

    Update Api_model.php

--- application/models/api_v2/Api_model.php
+++ application/models/api_v2/Api_model.php
@@ -53,6 +53,7 @@ class Api_model extends My_model {
         }
 
         $data['where']['vendor_id'] = $postData['vendor_id'];
+        $data['where']['status !='] '9';
         $data['table'] = 'user';
         $getUser = $this->selectRecords($data);
     
