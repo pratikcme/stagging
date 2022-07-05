7cc11012d7d5db4fe54fe709774931af39a7cfa8
shahidCME <sahid.cmexpertise@gmail.com>
2022-07-05 18:33:54 +0530

    Update Api_model.php

--- application/models/api_v2/Api_model.php
+++ application/models/api_v2/Api_model.php
@@ -1570,7 +1570,7 @@ class Api_model extends My_model {
                     if(!empty($isShow) && $isShow[0]->display_price_with_gst == '1'){
                         $product_weight_result[0]['discount_price'] = $product_weight_result[0]['without_gst_price'];
                     }   
-                     
+
                     $package_id = $product_weight_result[0]['package'];
                     $package_name = $this->get_package($package_id);
 
@@ -1611,6 +1611,7 @@ class Api_model extends My_model {
                 }
             $gettotal = $this->get_total($postdata);
             $getactual = $this->get_actual_total($postdata);
+            dd($getactual);
             $my_cal = $gettotal[0]->total;
             if ($my_cal === null || $my_cal == "<null>") {
                 $my_cal = 0.0;
