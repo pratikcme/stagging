55c52a09358a417b2fd0b308bdd1389d680248e2
shahidCME <sahid.cmexpertise@gmail.com>
2022-07-05 16:07:00 +0530

    Update Product_model.php

--- application/models/Product_model.php
+++ application/models/Product_model.php
@@ -510,7 +510,8 @@ public function Product_add_update(){
            $data['table'] =  TABLE_PRODUCT_WEIGHT;
            $data['update'] = ['without_gst_price'=>number_format((float)$product_price_without_gst, 2, '.', '') ];
            $data['where'] = ['id'=>$value->id];
-           $this->updateRecords($data);
+           // $this->updateRecords($data);
+           lq();
            // dd($data);
         }
     }   
