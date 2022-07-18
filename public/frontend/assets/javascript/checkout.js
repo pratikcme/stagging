2183c61ec3866688f8e7972c7196c9e76fed5773
shahidCME <sahid.cmexpertise@gmail.com>
2022-07-18 17:17:58 +0530

    Update checkout.js

--- public/frontend/assets/javascript/checkout.js
+++ public/frontend/assets/javascript/checkout.js
@@ -442,13 +442,9 @@ var CHECKOUT = function(){
                     finalAmount = (orderAmount + ( shipping_charge === "" ?  0  : parseFloat(shipping_charge) )- parseFloat(response.data)).toFixed(2)
                     // console.log("orderAmount ====" ,orderAmount ,  parseFloat(shipping_charge) ,  parseFloat(response.data))
                     var amount = (response.data).toFixed(2);
-                    alert(typeof(amount));
-                    alert(typeof(totalSaving));
                     if( $('#totalSaving').length ){
-                       var promocodeDiscount = totalSaving + amount;   
+                       var promocodeDiscount = parseFloat(totalSaving) + parseFloat(amount);   
                     }
-
-                    alert(promocodeDiscount);
                     $('#promoAmount').html(promocodeDiscount);
 
                     $('#checkout_final').html(finalAmount)                        
