2c451a7b84205007d03a985bbe64a034f8699f9c
shahidCME <sahid.cmexpertise@gmail.com>
2022-07-18 15:44:16 +0530

    Update Users_model.php

--- application/models/users_account/Users_model.php
+++ application/models/users_account/Users_model.php
@@ -439,13 +439,12 @@ class Users_model extends My_model {
 
     public function delete_user()
     {
-        
+
         $user_id = $this->session->userdata('user_id');
         $data['select'] = ['*'];
         $data['where'] = ['order_status <'=>'8','user_id'=>$user_id,'branch_id' =>$this->session->userdata('branch_id')];
         $data['table'] = TABLE_ORDER;
         $checkOrder = $this->selectRecords($data);
-        dd($checkOrder);
         if(!empty($checkOrder)){
             $this->utility->setFlashMessage('danger',"Please wait for deliver current order or cancle ongoing order");
             $response["success"] = 0;
