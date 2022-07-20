7be0861b27a9325154fc3946d6ed378e58e5e28a
shahidCME <sahid.cmexpertise@gmail.com>
2022-07-20 11:11:48 +0530

    Update Home.php

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -14,6 +14,7 @@ class Home extends User_Controller {
 	}
 
 	public function index(){
+		echo 'shukan';die;
 		$this->load->model('api_v3/common_model','co_model');
         $isShow = $this->co_model->checkpPriceShowWithGstOrwithoutGst($this->session->userdata('vendor_id'));
         // dd($isShow);
