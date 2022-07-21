0b780a11fe3b82f462d95b7e4f1878747a22b46d
shahidCME <sahid.cmexpertise@gmail.com>
2022-07-21 19:23:01 +0530

    Update offer_product_list.php

--- application/views/frontend/offer_product_list.php
+++ application/views/frontend/offer_product_list.php
@@ -12,7 +12,38 @@
     </div> 
 
     <div class="row" id="ajaxProduct">
-
+    	<div class="col-lg-3 col-md-6 col-sm-6">
+    		<div class="product-wrapper">
+    			
+    			<div class="wishlist-wrapper"><div class="">
+    			</div><div class="wishlist-icon" style="display:none" data-product_id="MTk0MQ" data-product_weight_id="MzkzOQ">
+    				<i class="far fa-heart "></i>
+    			</div>
+    		</div>
+    		<a href="https://bigbucket.launchestore.com/products/productDetails/MTk0MQ/MzkzOQ">
+    			<div class="feat-img">
+    				<img src="https://bigbucket.launchestore.com/public/images/bigbucket/product_image/product_image_1656672397.jpg">
+    			</div>
+    		</a>
+    		<div class="feature-detail">
+    			<a href="https://bigbucket.launchestore.com/products/productDetails/MTk0MQ/MzkzOQ"><h5>Beans</h5></a>
+    			<h6><span>₹</span> 20.00</h6>
+    			<p>Available (in stock)</p></div>
+    			<div class="feature-bottom-wrap">
+    				<div class="cart addcartbutton d-none" data-product_id="MTk0MQ">
+    					<i class="fas fa-shopping-basket"></i>
+    				</div>
+    				<div class="new_add_to_cart ">
+    					<button class="btn addcartbutton" data-product_id="MTk0MQ" data-varient_id="MzkzOQ">Add To Cart</button>
+    				</div>
+    				<div class="quantity-wrap d-none">
+    					<button class="dec cart-qty-minus" data-product_weight_id="3939"><span class="minus"><i class="fa fa-minus"></i></span></button>
+    					<input class="qty" type="text" name="" value="1" data-product_id="1941" data-weight_id="155" readonly="">
+    					<button class="inc cart-qty-plus" data-product_weight_id="3939"><span><i class="fa fa-plus"></i></span></button>
+    				</div>
+    			</div>
+    		</div>
+    	</div>
     </div>
    </div>
    <input type="hidden" name="" id="cat_id">
