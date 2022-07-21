7d9b176fbe4c27c287d641915c45252f95628021
shahidCME <sahid.cmexpertise@gmail.com>
2022-07-21 20:21:18 +0530

    Update Home.php

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -147,11 +147,12 @@ class Home extends User_Controller {
 		$data['js'] = array('add_to_cart.js');	
 		$postData = ['offer_id'=>$offer_id,'user_id' => $this->session->userdata('user_id')];
 		$data['offer_varient_list'] = $this->this_model->get_offer_varient_listing($postData);
-		
+
 		foreach ($data['offer_varient_list'] as $key => $value) {
-			$addQuantity = $this->product_model->findProductAddQuantity($value->id,$value->product_weight_id);
+			$addQuantity = $this->product_model->findProductAddQuantity($value->product_id,$value->product_weight_id);
   			$value->my_cart_quantity = $addQuantity;
 		}
+
 		dd($data['offer_varient_list']);		
 		$this->loadView(USER_LAYOUT,$data);
 	}
