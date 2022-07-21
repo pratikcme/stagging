a211f5ec96cf6932e9e687044c6ba3ca23c10e34
shahidCME <sahid.cmexpertise@gmail.com>
2022-07-21 20:02:38 +0530

    Update Home_model.php

--- application/models/frontend/Home_model.php
+++ application/models/frontend/Home_model.php
@@ -311,7 +311,6 @@ class Home_model extends My_model{
             }
             $data['table'] = 'my_cart';
             $result_cart = $this->selectRecords($data);
-            lq();
             if (count($result_cart) > 0) {
                 $my_cart_quantity = $result_cart[0]->quantity;
             } else {
