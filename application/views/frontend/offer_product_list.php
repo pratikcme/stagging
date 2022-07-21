42f274c2df34b34df90dd250e7733965c33f2b8c
shahidCME <sahid.cmexpertise@gmail.com>
2022-07-21 19:42:13 +0530

    Update offer_product_list.php

--- application/views/frontend/offer_product_list.php
+++ application/views/frontend/offer_product_list.php
@@ -12,42 +12,48 @@
     </div> 
 
     <div class="row" id="ajaxProduct">
+    	<?php foreach ($offer_varient_list as $key => $value) { ?>
     	<div class="col-lg-3 col-md-6 col-sm-6">
     		<div class="product-wrapper">
-    			
-    			<div class="wishlist-wrapper"><div class="">
-    			</div><div class="wishlist-icon" style="display:none" data-product_id="MTk0MQ" data-product_weight_id="MzkzOQ">
-    				<i class="far fa-heart "></i>
-    			</div>
-    		</div>
-    		<a href="https://bigbucket.launchestore.com/products/productDetails/MTk0MQ/MzkzOQ">
-    			<div class="feat-img">
-    				<img src="https://bigbucket.launchestore.com/public/images/bigbucket/product_image/product_image_1656672397.jpg">
-    			</div>
-    		</a>
-    		<div class="feature-detail">
-    			<a href="https://bigbucket.launchestore.com/products/productDetails/MTk0MQ/MzkzOQ"><h5>Beans</h5></a>
-    			<h6><span>₹</span> 20.00</h6>
-    			<p>Available (in stock)</p></div>
-    			<div class="feature-bottom-wrap">
-    				<div class="cart addcartbutton d-none" data-product_id="MTk0MQ">
-    					<i class="fas fa-shopping-basket"></i>
-    				</div>
-    				<div class="new_add_to_cart ">
-    					<button class="btn addcartbutton" data-product_id="MTk0MQ" data-varient_id="MzkzOQ">Add To Cart</button>
-    				</div>
-    				<div class="quantity-wrap d-none">
-    					<button class="dec cart-qty-minus" data-product_weight_id="3939"><span class="minus"><i class="fa fa-minus"></i></span></button>
-    					<input class="qty" type="text" name="" value="1" data-product_id="1941" data-weight_id="155" readonly="">
-    					<button class="inc cart-qty-plus" data-product_weight_id="3939"><span><i class="fa fa-plus"></i></span></button>
-    				</div>
-    			</div>
-    		</div>
+          <?php if($value->available_quantity == '0'){ ?>
+            <div class="out-stock"><span class="out-heading">out of stock</span></div>
+           <?php } ?>
+          <div class="wishlist-wrapper">
+            <?php if($value->new_percentage > '0'){ ?>
+            <div class="offer-wrap">
+              <p><?=$value->new_percentage.' % off'?></p>
+            </div>
+          <?php }else{ ?>
+            <div class="">
+            </div>
+          <?php } ?>
+            <div class="wishlist-icon" style="display: none" data-product_id="<?=$this->utility->safe_b64encode($value->product_id)?>"> <i class="far fa-heart"></i> </div>
+          </div>
+          <a href="<?=base_url().'products/productDetails/'.$this->utility->safe_b64encode($value->product_id).'/'.$this->utility->safe_b64encode($value->product_varient_id)?>">
+            <div class="feat-img"> <img class="lazy" data-src="<?=$value->image?>"> </div>
+          </a>
+          <div class="feature-detail">
+            <a href="<?=base_url().'products/productDetails/'.$this->utility->safe_b64encode($value->product_id).'/'.$this->utility->safe_b64encode($value->product_varient_id)?>">
+              <h5><?=$value->name?></h5> </a>
+            <h6><span><?=$this->siteCurrency?></span> <?=number_format((float)$value->discount_price, 2, '.', '')?></h6>
+            <p><?=($value->available_quantity >= 25 ) ? 'Available (in stock)' : 'limited stock' ?></p>
+          </div>
+
+          <div class="feature-bottom-wrap ">
+            <div class="cart addcartbutton d-none" data-product_id="<?=$this->utility->safe_b64encode($value->product_id)?>"> <i class="fas fa-shopping-basket"></i>
+            </div>
+            <div class="new_add_to_cart" >
+                  <button class="btn addcartbutton" data-product_id="<?=$this->utility->safe_b64encode($value->product_id)?>" data-varient_id="<?=$this->utility->safe_b64encode($value->product_varient_id)?>">Add To Cart</button>
+            </div>
+            <div class="quantity-wrap <?=$d_show?>">
+              <button class="dec cart-qty-minus" data-product_weight_id="<?=$value->product_varient_id?>"><span class="minus"><i class="fa fa-minus"></i></span></button>
+              <input class="qty" type="text" name="" value="<?=($value->my_cart_quantity != '0') ? $value->my_cart_quantity : 1 ?>" data-product_id="<?=$value->product_id?>" data-weight_id="<?=$value->weight_id?>" readonly>
+              <button class="inc cart-qty-plus" data-product_weight_id="<?=$value->product_varient_id?>"><span><i class="fa fa-plus"></i></span></button>
+            </div>
+          </div>
+        </div>
     	</div>
+    	<?php } ?>
     </div>
    </div>
-   <input type="hidden" name="" id="cat_id">
-   <input type="hidden" name="" id="sub_cat_id">
-   <input type="hidden" name="" id="getBycatID" value="<?=(isset($getBycatID) ?  $this->utility->safe_b64decode($getBycatID) : '' )?>">
-
 </section> 
