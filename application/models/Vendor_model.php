c3cf3fa62322e7f1f7263a0ba5fab37bc7232681
shahidCME <sahid.cmexpertise@gmail.com>
2022-07-21 14:44:28 +0530

    Update Vendor_model.php

--- application/models/Vendor_model.php
+++ application/models/Vendor_model.php
@@ -88,6 +88,7 @@ class vendor_model extends My_model{
                         // $image = $uploadResponse['data']['file_name'];
                     }
                     $logo = '';
+                    dd($_FILES['logo_image']);
                     if(isset($_FILES['logo_image'])){
                         $logo_upload_path = "./public/images/".$this->folder."vendor_logo_image";
                         $uploadLogoImage = upload_single_image_ByName($_FILES,'logo_image',$logo_upload_path);
