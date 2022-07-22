912b9f9f4d6e2ea12da525975be3f5fae149a51e
shahidCME <sahid.cmexpertise@gmail.com>
2022-07-22 13:24:48 +0530

    da

--- application/views/product_profile.php
+++ application/views/product_profile.php
@@ -1,6 +1,4 @@
 <?php
-    error_reporting(E_ALL);
-            ini_set('display_errors', 1);
 include('header.php');
 $id = $this->utility->decode($_GET['id']);
 $branch_id = $this->session->userdata['id'];
