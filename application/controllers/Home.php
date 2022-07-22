353f9b04155bc72017aa85bea1a6d22694d6841d
shahidCME <sahid.cmexpertise@gmail.com>
2022-07-22 12:08:45 +0530

    Revert "offer-branch wise in web"
    This reverts commit 20325b31846f683ef230e3ea74eb8ca8c4801e2c.

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -120,7 +120,7 @@ class Home extends User_Controller {
 
 		
 		$data['item_weight_id'] = $item_weight_id ;
-		$data['offer_list'] = $this->this_model->get_offer($this->session->userdata('branch_id'));
+		$data['offer_list'] = $this->this_model->get_offer($this->session->userdata('vendor_id'));
 		// dd($data['offer_list']);
 		$this->loadView(USER_LAYOUT,$data);
 	
